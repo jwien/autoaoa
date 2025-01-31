ce1252be4f3f6641c7545ca74cb4483019a8d380
James Wienecke <james.a.wienecke@gmail.com>
2025-01-31 08:48:44 -0600

    Added and configured plugin html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
   entry: './src/index.ts',
@@ -32,4 +33,8 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [new HtmlWebpackPlugin({
+    title: 'TTT AutoAOA',
+    template: '/src/html/index.html'
+  })],
 };
