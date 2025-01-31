af37f82379c066345ece4c4c3b80ab85672df72e
James Wienecke <james.a.wienecke@gmail.com>
2025-01-31 12:03:52 -0600

    Some refactoring while trying to fix (#1).

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import { PDFDocument } from 'pdf-lib';
 import aoaPdf from './docs/AOA.pdf';
 import mapToPdfFieldNames, { aoaInfo, aoaPropertyInfo } from './mapToPdfFields';
 
-
 const newAoa: aoaInfo = {
     cadname: "TRAVIS",
     ownername: "John Smith",
@@ -22,19 +21,17 @@ async function modifyPdf(aoa: aoaInfo) {
 
     const aoaIframe = <HTMLIFrameElement> document.getElementById('aoa-iframe');
     aoaIframe.className = "d-block";
-    console.log(pdfDataUri);
     aoaIframe.src = pdfDataUri;
 
     aoaIframe.scrollIntoView({ behavior: "smooth" })
-
 }
 
 function handleUserInput() {
-
     const clientInfoSubmit = <HTMLButtonElement> document.getElementById('clientinfo-submit');
-    clientInfoSubmit.addEventListener("click", event => {
+    clientInfoSubmit.addEventListener("click", async event => {
         event.preventDefault();
-
+        clientInfoSubmit.removeEventListener("click", null)
+        
         const clientInfoElement = <HTMLFormElement> document.getElementById('clientinfo');
         const clientInfoFormData = new FormData(clientInfoElement);
 
@@ -42,40 +39,58 @@ function handleUserInput() {
         newAoa.ownername = clientInfoFormData.get("inputOwnername").toString();
         newAoa.address = clientInfoFormData.get("inputHomeaddress").toString();
         newAoa.citystatezip = clientInfoFormData.get("inputCitystatezip").toString();
-
-        console.log("prompt start")
-        const needsMoreProperties = confirm("Do you have other properties you would like to protest?");
-        console.log("prompt end")
-        clientInfoSubmit.removeEventListener("click", null)
-
-        if (needsMoreProperties) {
-            const propertyInfoElement = <HTMLFormElement> document.getElementById("propertyinfo");
-            propertyInfoElement.className = "d-block";
-
-            propertyInfoElement.scrollIntoView({ behavior: 'smooth' });
-            const firstElementField = document.getElementsByName("inputPropaddress0").item(0);
-            firstElementField.focus();
-    
-            const propInfoSubmit = <HTMLButtonElement> document.getElementById('propertyinfo-submit');
-            propInfoSubmit.addEventListener("click", event => {
-                event.preventDefault();
-    
-                const propInfoFormData = new FormData(propertyInfoElement);
-                
-                console.log(propInfoFormData);
-                for (let i = 0; i < 4; i++) {
-                    const property: aoaPropertyInfo = {
-                        situsaddress: propInfoFormData.get("inputPropaddress" + i).toString(),
-                        pid: propInfoFormData.get("inputProppid" + i).toString(),
-                        legaldescription: propInfoFormData.get("inputProplegaldesc" + i).toString(),
+        const needsMoreProperties = new Promise<boolean>((res, rej) => {
+            const morePropertiesModal = <HTMLElement> document.getElementById("more-properties-prompt")
+            const morePropertiesModalBtnFalse = <HTMLElement> document.getElementById("modal-btn-false")
+            const morePropertiesModalBtnTrue = <HTMLElement> document.getElementById("modal-btn-true")
+            
+            if (morePropertiesModal && morePropertiesModalBtnFalse && morePropertiesModalBtnTrue) {
+                morePropertiesModal.addEventListener('shown.bs.modal', () => morePropertiesModalBtnFalse.focus())
+                morePropertiesModalBtnFalse.addEventListener("click", (event) => {
+                    if (event.target instanceof HTMLElement) {
+                        const value = Boolean(event.target.dataset.val);
+                        res(value);
                     }
-                    newAoa.properties.push(property);
-                }
+                })
+                morePropertiesModalBtnTrue.addEventListener("click", (event) => {
+                    if (event.target instanceof HTMLElement) {
+                        const value = Boolean(event.target.dataset.val);
+                        res(value);
+                    }
+                })
+            }
+        });
+
+        await needsMoreProperties.then((doesNeed) => {
+            if (doesNeed) {
+                const propertyInfoElement = <HTMLFormElement> document.getElementById("propertyinfo");
+                propertyInfoElement.className = "d-block";
+    
+                propertyInfoElement.scrollIntoView({ behavior: 'smooth' });
+                const firstElementField = document.getElementsByName("inputPropaddress0").item(0);
+                firstElementField.focus();
+        
+                const propInfoSubmit = <HTMLButtonElement> document.getElementById('propertyinfo-submit');
+                propInfoSubmit.addEventListener("click", event => {
+                    event.preventDefault();
+        
+                    const propInfoFormData = new FormData(propertyInfoElement);
+                    
+                    for (let i = 0; i < 4; i++) {
+                        const property: aoaPropertyInfo = {
+                            situsaddress: propInfoFormData.get("inputPropaddress" + i).toString(),
+                            pid: propInfoFormData.get("inputProppid" + i).toString(),
+                            legaldescription: propInfoFormData.get("inputProplegaldesc" + i).toString(),
+                        }
+                        newAoa.properties.push(property);
+                    }
+                    modifyPdf(newAoa);
+                });
+            } else {
                 modifyPdf(newAoa);
-            });
-        } else {
-            modifyPdf(newAoa);
-        }
+            }
+        })
+
     }, { once: true });
 };
 
